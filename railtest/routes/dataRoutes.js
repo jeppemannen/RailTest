a1c91c3792791639c59f9e2d1e4e48af742ce6a6
jeppe <jens.s.opsvik@gmail.com>
2025-05-24 13:14:49 +0200

    3

--- railtest/routes/dataRoutes.js
+++ railtest/routes/dataRoutes.js
@@ -4,8 +4,13 @@ const Item = require("../models/item");
 
 // GET all items
 router.get('/', async (req, res) => {
-  const items = await Item.find();
-  res.json(items);
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    console.error('Error fetching items:', err.message);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 module.exports = router;
